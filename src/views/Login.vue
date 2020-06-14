235850950e323a149191c5fb6dc26c3ac1c47980
HerrBaumx <731278859@qq.com>
2020-06-14 18:36:39 +0800

    登录页面制作，跳转

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -42,6 +42,8 @@
                         postKeyValueRequest("/doLogin",this.loginForm).then(resp=>{
                             if(resp){
                                 alert(JSON.stringify(resp))
+                                window.sessionStorage.setItem("user", JSON.stringify(resp.obj));
+                                this.$router.replace("/home");
                             }
                         })
                         alert('submit!');
