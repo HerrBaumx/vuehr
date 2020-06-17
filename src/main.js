076e02ad15e7c82bb6c2203e151cfb3e4fb0f333
HerrBaumx <731278859@qq.com>
2020-06-17 13:34:28 +0800

    Vuex 配置

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
@@ -21,6 +22,7 @@ Vue.config.productionTip = false
 Vue.use(ElementUI);
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
