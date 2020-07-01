8657af59483dcdf5375ee4234c5826bb765b1a3b
HerrBaumx <731278859@qq.com>
2020-07-01 20:11:59 +0800

    职称管理批量删除实现

--- src/components/sys/basic/JobLevelMana.vue
+++ src/components/sys/basic/JobLevelMana.vue
@@ -20,7 +20,12 @@
                     :data="jls"
                     border
                     size="small"
-                    style="width: 80%">
+                    style="width: 80%"
+                    @selection-change="handleSelectionChange">
+                <el-table-column
+                        type="selection"
+                        width="55">
+                </el-table-column>
                 <el-table-column
                         prop="id"
                         label="编号"
@@ -57,7 +62,10 @@
                     </template>
                 </el-table-column>
             </el-table>
-
+            <el-button type="danger" size="small" style="margin-top: 10px" :disabled="multipleSelection.length==0"
+                       @click="deleteMany">
+                批量删除
+            </el-button>
         </div>
         <el-dialog
                 title="修改职称"
@@ -102,6 +110,7 @@
                         </td>
                     </tr>
                 </table>
+
             </div>
             <span slot="footer" class="dialog-footer">
     <el-button size="small" @click="dialogVisible = false">取 消</el-button>
@@ -116,6 +125,7 @@
         name: "JobLevelMana",
         data() {
             return {
+                multipleSelection: [],
                 dialogVisible: false,
                 updateJl: {
                     name: '',
@@ -140,6 +150,32 @@
             this.initJls();
         },
         methods: {
+            deleteMany() {
+                this.$confirm('此操作将永久删除【' + this.multipleSelection.length + '】条记录, 是否继续?', '提示', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    type: 'warning'
+                }).then(() => {
+                    let ids = '?';
+                    this.multipleSelection.forEach(item => {
+                        ids += 'ids=' + item.id + '&';
+                    });
+                    this.deleteRequest("/system/basic/joblevel/" + ids).then(resp => {
+                        if (resp) {
+                            this.initJls();
+                        }
+                    });
+                }).catch(() => {
+                    this.$message({
+                        type: 'info',
+                        message: '已取消删除'
+                    });
+                });
+            },
+
+            handleSelectionChange(val) {
+                this.multipleSelection = val;
+            },
             doUpdate() {
                 this.putRequest("/system/basic/joblevel/", this.updateJl).then(resp => {
                     if (resp) {
