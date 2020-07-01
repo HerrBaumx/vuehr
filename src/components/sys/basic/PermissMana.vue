fc5b6f698e1628757cabc8235ab49b4b32f3922b
HerrBaumx <731278859@qq.com>
2020-07-01 20:54:58 +0800

    权限组前端页面制作

--- src/components/sys/basic/PermissMana.vue
+++ src/components/sys/basic/PermissMana.vue
@@ -1,15 +1,53 @@
 <template>
     <div>
-        权限组
+        <div class="permissManaTool">
+            <el-input size="small" placeholder="请输入角色英文名" v-model="role.name">
+                <template slot="prepend">ROLE_</template>
+            </el-input>
+            <el-input size="small" placeholder="请输入角色中文名" v-model="role.nameZh"></el-input>
+            <el-button size="small" type="primary" icon="el-icon-plus">添加角色</el-button>
+        </div>
+        <div class="permissManaMain">
+            <el-collapse v-model="activeName" accordion>
+                <el-collapse-item title="一致性 Consistency" name="1">
+                    <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
+                    <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+                </el-collapse-item>
+            </el-collapse>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "PermissMana"
+        name: "PermissMana",
+        data() {
+            return {
+                activeName: '2',
+                role: {
+                    name: '',
+                    nameZh: ''
+                }
+            }
+        }
     }
 </script>
 
-<style scoped>
+<style>
+    .permissManaMain {
+        margin-top: 10px;
+        width: 700px
+    }
+
+    .permissManaTool {
+        display: flex;
+        justify-content: flex-start;
+    }
+
+    .permissManaTool .el-input {
+        width: 300px;
+        margin-right: 6px;
+
+    }
 
 </style>
