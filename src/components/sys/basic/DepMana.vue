66c4341ff2eec94f7a48920d106898c08a7588ea
HerrBaumx <731278859@qq.com>
2020-07-05 21:58:56 +0800

    部门管理部门树动态添加

--- src/components/sys/basic/DepMana.vue
+++ src/components/sys/basic/DepMana.vue
@@ -34,6 +34,33 @@
         </span>
       </span>
         </el-tree>
+        <el-dialog
+                title="添加部门"
+                :visible.sync="dialogVisible"
+                width="30%">
+
+            <table>
+                <tr>
+                    <td>
+                        <el-tag>上级部门</el-tag>
+                    </td>
+                    <td>{{pname}}</td>
+                </tr>
+                <tr>
+                    <td>
+                        <el-tag>部门名称</el-tag>
+                    </td>
+                    <td>
+                        <el-input v-model="dep.name" placeholder="请输入部门名称..."></el-input>
+                    </td>
+                </tr>
+
+            </table>
+            <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="doAddDep">确 定</el-button>
+  </span>
+        </el-dialog>
     </div>
 </template>
 
@@ -42,8 +69,14 @@
         name: "DepMana",
         data() {
             return {
+                dialogVisible: false,
                 filterText: '',
                 deps: [],
+                dep: {
+                    name: '',
+                    parentId: -1
+                },
+                pname: '',
                 defaultProps: {
                     children: 'children',
                     label: 'name'
@@ -59,13 +92,46 @@
             this.initDeps();
         },
         methods: {
+            initDep() {
+                this.dep = {
+                    name: '',
+                    parentId: -1
+                };
+                this.pname = '';
+            },
+
+            addDep2Deps(deps, dep) {
+                for (let i = 0; i < deps.length; i++) {
+                    let d = deps[i];
+                    if (d.id == dep.parentId) {
+                        d.children = d.children.concat(dep);
+                        if (d.children.length > 0) {
+                            d.parent = true;
+                        }
+                        return;
+                    } else {
+                        this.addDep2Deps(d.children, dep);
+                    }
+                }
+            },
+            doAddDep() {
+                this.postRequest("/system/basic/department/" ,this.dep).then(resp => {
+                    if (resp) {
+                        this.addDep2Deps(this.deps, resp.obj);
+                        this.dialogVisible = false;
+                        this.initDep();
+                    }
+                });
+            },
             deleteDep(data) {
                 console.log(data);
             },
 
 
             showAddDepView(data) {
-                console.log(data);
+                this.pname = data.name;
+                this.dep.parentId = data.id;
+                this.dialogVisible = true;
             },
             initDeps() {
                 this.getRequest("/system/basic/department/").then(resp => {
