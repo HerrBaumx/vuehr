5d8c95fdc9e2a5b31c041aa9671a7948817e58a6
HerrBaumx <731278859@qq.com>
2020-07-21 23:19:38 +0800

    项目首页聊天页面绘制

---
+++ src/components/chat/card.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="card">
+  	<header>
+  		<img class="avatar" v-bind:src="user.img" v-bind:alt="user.name">
+  		<p class="name">{{user.name}}</p>
+  	</header>
+  	<footer>
+  		<input class="search" type="text" v-model="$store.state.filterKey" placeholder="search user...">
+  	</footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'card',
+  data () {
+    return {
+      user: {
+      	name: 'Li-yiwei',
+      	img: 'https://t7.baidu.com/it/u=3616242789,1098670747&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1595948848&t=5b498b2e7a1c0b281c8552191c0981bb'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#card {
+	padding: 12px;
+  .avatar{
+  	width: 40px;
+  	height: 40px;
+  	vertical-align: middle;/*这个是图片和文字居中对齐*/
+  }
+  .name {
+  	display: inline-block;
+  	padding: 10px;
+  	margin-bottom: 15px;
+  	font-size: 16px;
+  }
+  .search {
+  	background: #26292E;
+  	height: 30px;
+  	line-height: 30px;
+  	padding: 0 10px;
+  	border: 1px solid #3a3a3a;
+  	border-radius: 4px;
+  	outline: none;/*鼠标点击后不会出现蓝色边框*/
+    color: #FFF;
+  }
+}
+</style>
