278babe232c0b5193e8c409168f4e6e93235c3bc
HerrBaumx <731278859@qq.com>
2020-08-03 21:55:37 +0800

    个人中心数据修改

--- src/views/HrInfo.vue
+++ src/views/HrInfo.vue
@@ -23,10 +23,55 @@
                 </el-tag>
             </div>
             <div style="display: flex;justify-content: space-around;margin-top: 10px">
-                <el-button type="primary">修改信息</el-button>
+                <el-button type="primary" @click="showUpdateHrInfoView">修改信息</el-button>
                 <el-button type="danger">修改密码</el-button>
             </div>
         </el-card>
+        <el-dialog
+                title="修改用户信息"
+                :visible.sync="dialogVisible"
+                width="30%">
+            <div>
+                <table>
+                    <tr>
+                        <td>
+                            <el-tag>用户昵称：</el-tag>
+                        </td>
+                        <td>
+                            <el-input v-model="hr2.name"></el-input>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <el-tag>电话号码：</el-tag>
+                        </td>
+                        <td>
+                            <el-input v-model="hr2.telephone"></el-input>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <el-tag>手机号码：</el-tag>
+                        </td>
+                        <td>
+                            <el-input v-model="hr2.phone"></el-input>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <el-tag>用户地址：</el-tag>
+                        </td>
+                        <td>
+                            <el-input v-model="hr2.address"></el-input>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="updateHrInfo">确 定</el-button>
+  </span>
+        </el-dialog>
     </div>
 </template>
 
@@ -35,17 +80,34 @@
         name: "HrInfo",
         data() {
             return {
-                hr: null
+                hr: null,
+                hr2: null,
+                dialogVisible: false
             };
         },
         mounted() {
             this.initHr();
         },
         methods: {
+
+            updateHrInfo() {
+                this.putRequest("/hr/info", this.hr2).then(resp => {
+                    if (resp) {
+                        this.dialogVisible = false;
+                        this.initHr();
+                    }
+                });
+            },
+            showUpdateHrInfoView() {
+                this.dialogVisible = true;
+            },
             initHr() {
                 this.getRequest('/hr/info').then(resp => {
                     if (resp) {
                         this.hr = resp;
+                        this.hr2 = Object.assign({}, this.hr);
+                        window.sessionStorage.setItem("user", JSON.stringify(resp));
+                        this.$store.commit("INIT_CURRENTHR", resp);
                     }
                 });
             }
